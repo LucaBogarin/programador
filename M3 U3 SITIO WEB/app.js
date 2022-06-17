99f23abb6df580a676d09bc8569d6662a35908a5
Luca Bogarin <lucabogarin9@gmail.com>
2022-06-17 16:43:46 -0300

    17/06 2

--- M3 U3 SITIO WEB/app.js
+++ M3 U3 SITIO WEB/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 require('dotenv').config();
 var session = require('express-session');
 
+var fileUpload = require('express-fileupload');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
@@ -29,6 +31,8 @@ app.use(session({
   saveUninitialized: true
 }))
 
+
+
 secured = async (req,res,next)=> {
   try{
     //console.log(req.session.id_usuario);
@@ -41,6 +45,11 @@ secured = async (req,res,next)=> {
     console.log(error);
   }
 }
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
