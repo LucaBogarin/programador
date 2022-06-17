405ebc5dccdc7f591ab8d63a3df2b37023959e7d
Luca Bogarin <lucabogarin9@gmail.com>
2022-06-17 13:08:15 -0300

    17/06

--- M3 U3 SITIO WEB/models/novedadesModel.js
+++ M3 U3 SITIO WEB/models/novedadesModel.js
@@ -12,5 +12,16 @@ var rows = await pool.query(query, [id]);
 return rows;
 }
 
+async function insertNovedad(obj){
+    try{
+        var query = "insert into novedades set ?";
+        var rows = await pool.query(query, [obj]);
+        return rows;
+    }catch(error){
+        console.log(error);
+        throw error;
+    }
+}
 
-module.exports = {getNovedades, deleteNovedadesById}
+
+module.exports = {getNovedades, deleteNovedadesById,insertNovedad}
